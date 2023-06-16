df991ac00b3636bc3d2d398777504b8f8edce7a6
Jide_O <45870109+JideOgunlana@users.noreply.github.com>
2023-06-16 14:23:11 +0000

    currency option added

--- app.js
+++ app.js
@@ -12,11 +12,24 @@ let itemArr = [];
 let list = document.querySelector(".list");
 let incomeSum = 0;
 let expenseSum = 0;
+let currencyForm = document.querySelector(".currency_form");
+
 
 let deletedvalue;
 let undeletedvalue = 0;
-totalIncome.innerHTML = "&#8358; 0";
-totalExpenses.innerHTML = "&#8358; 0";
+
+let currency = "USD";
+let prevCurrency = currency;
+let currencyFormatter = new Intl.NumberFormat(
+    'en-US', {
+        style: "currency",
+        currency:  `${currency}`,
+        maximumFractionDigits: 2
+    }
+);
+
+totalIncome.innerHTML = `${currencyFormatter.format(0)}`;
+totalExpenses.innerHTML = `${currencyFormatter.format(0)}`;
 
 class Item {
     constructor(desc, income, expense) {
@@ -26,37 +39,94 @@ class Item {
     }
 }
 
+currencyForm.addEventListener('change', (e) => {
+    console.log(e.target.value);
+    currency = e.target.value;
+    currencyChanged(currency);
+})
+
+let currencyChanged = (currency) => {
+    currencyFormatter = new Intl.NumberFormat(
+        'en-US', {
+            currencyDisplay: "narrowSymbol",
+            style: "currency",
+            currency: currency,
+            maximumFractionDigits: 2
+        }
+    );
+    let newTotalIncome = totalIncome.textContent.slice(1);
+    let newTotalExpense = totalExpenses.textContent.slice(1);
+    totalIncome.innerHTML = `${currencyFormatter.format(newTotalIncome)} `;
+    totalExpenses.innerHTML = `${currencyFormatter.format(myExpenseTotal(newTotalExpense))} `;
+
+    if (currency !== prevCurrency) {
+        console.log(list.children);
+        prevCurrency = currency;
+        if (list.children.length < 1)
+            return ;
+        let index = 0;
+        list.childNodes.forEach((item) => {
+            // console.log(item.innerText);
+            let dupItemValue = item.innerText;
+            // console.log(dupItemValue);
+            console.log(item, item.innerText);
+            if (dupItemValue) {
+                let mod = dupItemValue.split(" ");
+                console.log(mod);
+                if (itemArr[index].income != "") {
+                    mod[1] = `${currencyFormatter.format(itemArr[index].income)}`;            
+                    item.innerHTML = `<span class="a">X</span>${mod[0]} ${mod[1]}`;
+                }
+                else {
+                    mod[1] = `${currencyFormatter.format(itemArr[index].expense)}`;            
+                    item.innerHTML = `<span class="a">X</span>${mod[0]} ${mod[1]}`;
+                }
+                console.log(mod);
+                index++;
+            }
+        });
+    }
+}
+
 btn.addEventListener('click', (x) => {
+    let validValue = false;
     // checks if the description field is not empty and at least one of the other fields is not
     if (itemDesc.value != "" && (income.value != "" || expense.value != ""))
     {
         console.log(itemDesc.value, income.value, expense.value);
     //  Checks each field individually and adds to its array values that are not an empty string
-        if (itemDesc.value != "")
-        {
+        // if (itemDesc.value != "")
+        // {
             // items.desc = itemDesc.value;
             // items.income = income.value;
             // items.expense = expense.value;
             // itemArr.push(items);
-            itemArr.push(new Item(itemDesc.value, income.value, expense.value)); //creates an object for new entries passed to the array of item
-        }
-        if (income.value != "")
+            // }
+        if (income.value != "" && parseFloat(income.value) > 0)
         {
+            itemArr.push(new Item(itemDesc.value, income.value, expense.value)); //creates an object for new entries passed to the array of item
             incomeArr.push(income.value); // array of values entered in the income field
             // adds to the list an income
-            list.insertAdjacentHTML("beforeend", `<li class="income-li"><span class="a">X</span> ${itemDesc.value} &nbsp; &#8358; ${income.value}</li>`);
+            list.insertAdjacentHTML("beforeend", `<li class="income-li"><span class="a">X</span>${itemDesc.value} ${currencyFormatter.format(income.value)}</li>`);
+            validValue = true;
         }
-        if (expense.value != "")
+        console.log(expense.value);
+        if (expense.value != "" && parseFloat(expense.value) > 0)
         {
+            itemArr.push(new Item(itemDesc.value, income.value, expense.value)); //creates an object for new entries passed to the array of item
             expenseArr.push(expense.value); // array of values entered in the expense field
             // adds to the list an expense 
-            list.insertAdjacentHTML("beforeend", `<li class="expense-li"><span class="a">X</span> ${itemDesc.value} &nbsp; &#8358; ${expense.value}</li>`);
+            list.insertAdjacentHTML("beforeend", `<li class="expense-li"><span class="a">X</span>${itemDesc.value} ${currencyFormatter.format(expense.value)}</li>`);
+            validValue = true;
         }
         // helper fxns are invoked and total income and total expenses are updated
-        totalIncome.innerHTML = "&#8358; " + myIncomeTotal(income.value, expense.value);
-        totalExpenses.innerHTML = "&#8358; " +  myExpenseTotal(expense.value);
-        // totalIncome.insertAdjacentHTML("beforeend", `<div>${myIncomeTotal(income.value, expense.value)}</div>`)
-        console.log(itemArr, incomeArr, expenseArr);
+        if (validValue) {
+            totalIncome.innerHTML = `${currencyFormatter.format(myIncomeTotal(income.value, expense.value))} `;
+            totalExpenses.innerHTML = `${currencyFormatter.format(myExpenseTotal(expense.value))} `;
+            // totalIncome.insertAdjacentHTML("beforeend", `<div>${myIncomeTotal(income.value, expense.value)}</div>`)
+            // console.log(itemArr, incomeArr, expenseArr);
+            validValue = false;
+        }
 
         // clears either income or expense field after submission
         if((income.value != "") || (expense.value != ""))
@@ -149,7 +219,7 @@ list.addEventListener('click', function (e) {
                 console.log(incomeArr[i]);
                 deletedvalue = incomeArr[i];
                 incomeArr.splice(i,1);
-                totalIncome.innerHTML = "&#8358; " + myIncomeTotal(-deletedvalue,undeletedvalue);
+                totalIncome.innerHTML = `${currencyFormatter.format(myIncomeTotal(-deletedvalue,undeletedvalue))}`;
             }
         }
 
@@ -176,8 +246,8 @@ list.addEventListener('click', function (e) {
                 console.log(expenseArr[i]);
                 deletedvalue = expenseArr[i];
                 expenseArr.splice(i,1);
-                totalIncome.innerHTML = "&#8358; " + myIncomeTotal(undeletedvalue,-deletedvalue);
-                totalExpenses.innerHTML = "&#8358; " + myExpenseTotal(-deletedvalue);
+                totalIncome.innerHTML = `${currencyFormatter.format( myIncomeTotal(undeletedvalue,-deletedvalue) )}`;
+                totalExpenses.innerHTML = `${currencyFormatter.format( myExpenseTotal(-deletedvalue) )}`;
             }
         }
         console.log(myarray);

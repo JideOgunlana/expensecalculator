9368610086685a17d11a395a8d64f99f3d0e3251
Jide_O <45870109+JideOgunlana@users.noreply.github.com>
2023-06-17 19:28:11 +0000

    fixes added

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ let list = document.querySelector(".list");
 let incomeSum = 0;
 let expenseSum = 0;
 let currencyForm = document.querySelector(".currency_form");
+let wallet = document.querySelector(".wallet");
+let incomeTotal = 0;
 
 
 let deletedvalue;
@@ -30,6 +32,7 @@ let currencyFormatter = new Intl.NumberFormat(
 
 totalIncome.innerHTML = `${currencyFormatter.format(0)}`;
 totalExpenses.innerHTML = `${currencyFormatter.format(0)}`;
+wallet.innerHTML = `${currencyFormatter.format(0)}`;
 
 class Item {
     constructor(desc, income, expense) {
@@ -40,7 +43,6 @@ class Item {
 }
 
 currencyForm.addEventListener('change', (e) => {
-    console.log(e.target.value);
     currency = e.target.value;
     currencyChanged(currency);
 })
@@ -56,32 +58,33 @@ let currencyChanged = (currency) => {
     );
     let newTotalIncome = totalIncome.textContent.slice(1);
     let newTotalExpense = totalExpenses.textContent.slice(1);
+    let yourBalanace = wallet.textContent.slice(1);
     totalIncome.innerHTML = `${currencyFormatter.format(newTotalIncome)} `;
-    totalExpenses.innerHTML = `${currencyFormatter.format(myExpenseTotal(newTotalExpense))} `;
+    totalExpenses.innerHTML = `${currencyFormatter.format(newTotalExpense)} `;
+    wallet.innerHTML = `${currencyFormatter.format(yourBalanace)} `;
 
     if (currency !== prevCurrency) {
-        console.log(list.children);
         prevCurrency = currency;
         if (list.children.length < 1)
             return ;
         let index = 0;
         list.childNodes.forEach((item) => {
-            // console.log(item.innerText);
             let dupItemValue = item.innerText;
-            // console.log(dupItemValue);
-            console.log(item, item.innerText);
+            
             if (dupItemValue) {
                 let mod = dupItemValue.split(" ");
-                console.log(mod);
                 if (itemArr[index].income != "") {
-                    mod[1] = `${currencyFormatter.format(itemArr[index].income)}`;            
+                    mod[1] = `${currencyFormatter.format(itemArr[index].income)}`;
                     item.innerHTML = `<span class="a">X</span>${mod[0]} ${mod[1]}`;
+                    totalIncome.innerHTML = currencyFormatter.format(incomeTotal);
+                    wallet.innerHTML = currencyFormatter.format(incomeSum);
                 }
                 else {
-                    mod[1] = `${currencyFormatter.format(itemArr[index].expense)}`;            
+                    mod[1] = `${currencyFormatter.format(itemArr[index].expense)}`;
                     item.innerHTML = `<span class="a">X</span>${mod[0]} ${mod[1]}`;
+                    totalExpenses.innerHTML = currencyFormatter.format(expenseSum);
+                    wallet.innerHTML = currencyFormatter.format(incomeSum);
                 }
-                console.log(mod);
                 index++;
             }
         });
@@ -93,7 +96,6 @@ btn.addEventListener('click', (x) => {
     // checks if the description field is not empty and at least one of the other fields is not
     if (itemDesc.value != "" && (income.value != "" || expense.value != ""))
     {
-        console.log(itemDesc.value, income.value, expense.value);
     //  Checks each field individually and adds to its array values that are not an empty string
         // if (itemDesc.value != "")
         // {
@@ -110,7 +112,6 @@ btn.addEventListener('click', (x) => {
             list.insertAdjacentHTML("beforeend", `<li class="income-li"><span class="a">X</span>${itemDesc.value} ${currencyFormatter.format(income.value)}</li>`);
             validValue = true;
         }
-        console.log(expense.value);
         if (expense.value != "" && parseFloat(expense.value) > 0)
         {
             itemArr.push(new Item(itemDesc.value, income.value, expense.value)); //creates an object for new entries passed to the array of item
@@ -121,10 +122,10 @@ btn.addEventListener('click', (x) => {
         }
         // helper fxns are invoked and total income and total expenses are updated
         if (validValue) {
-            totalIncome.innerHTML = `${currencyFormatter.format(myIncomeTotal(income.value, expense.value))} `;
+            totalIncome.innerHTML = `${currencyFormatter.format(myIncome(income.value))} `;
+            wallet.innerHTML = `${currencyFormatter.format(myIncomeTotal(income.value, expense.value))}`
             totalExpenses.innerHTML = `${currencyFormatter.format(myExpenseTotal(expense.value))} `;
             // totalIncome.insertAdjacentHTML("beforeend", `<div>${myIncomeTotal(income.value, expense.value)}</div>`)
-            // console.log(itemArr, incomeArr, expenseArr);
             validValue = false;
         }
 
@@ -142,8 +143,12 @@ btn.addEventListener('click', (x) => {
 //calculates the total income 
 let myIncomeTotal = (myIncome, mySpendings) => {
     incomeSum = incomeSum + (myIncome - mySpendings) ;
-    console.log(incomeSum); 
     return incomeSum;  
+}
+
+let myIncome = (myIncome) => {
+    incomeTotal += (myIncome - 0);
+    return incomeTotal;
 }
 
 // calculates the total expenses
@@ -174,21 +179,15 @@ function disableInput() {
 
 // deletes any item on the list and updates the total income
 list.addEventListener('click', function (e) {
-    console.log(e.target);
-    console.log(list.children);
-    // console.log(e.target.parentElement)
     //checks the targeted span tag to delete the li element containing it 
     if (e.target.attributes.class.value == "a")
     {
         // e.target.parentNode.remove()
-        console.log(e.target.parentNode);
         // if(e.target.parentNode.attributes.class.value =="income-li")
         // {
-        //     console.log("Income");
         // }
         // else if (e.target.parentNode.attributes.class.value == "expense-li")
         // {
-        //     console.log("Expenditure")
         // }
 
         let i = 0;
@@ -196,10 +195,8 @@ list.addEventListener('click', function (e) {
         let myexparray = []; // works the same way to delete and deduct the targeted expense
         while (i < list.children.length)
         {
-            // console.log(list.children[i]);
             if (list.children[i].attributes.class.value == "income-li")
             {
-                console.log(list.children[i]);
                 myarray.push(list.children[i]);
                 // to delete item removed from the item arr
                     if(list.children[i] == e.target.parentNode)
@@ -215,21 +212,18 @@ list.addEventListener('click', function (e) {
         {
             if (myarray[i] == e.target.parentNode)
             {
-                console.log(i);
-                console.log(incomeArr[i]);
                 deletedvalue = incomeArr[i];
                 incomeArr.splice(i,1);
-                totalIncome.innerHTML = `${currencyFormatter.format(myIncomeTotal(-deletedvalue,undeletedvalue))}`;
+                totalIncome.innerHTML = `${currencyFormatter.format(myIncomeTotal(-deletedvalue))}`;
+                wallet.innerHTML = `${currencyFormatter.format(myIncomeTotal(-deletedvalue,undeletedvalue))}`;
             }
         }
 
         let j = 0;
         while (j < list.children.length)
         {
-            // console.log(list.children[i]);
             if (list.children[j].attributes.class.value == "expense-li")
             {
-                console.log(list.children[j])
                 myexparray.push(list.children[j]);
                 if(list.children[j] == e.target.parentNode)
                 {
@@ -242,15 +236,13 @@ list.addEventListener('click', function (e) {
         {
             if (myexparray[i] == e.target.parentNode)
             {
-                console.log(i);
-                console.log(expenseArr[i]);
                 deletedvalue = expenseArr[i];
                 expenseArr.splice(i,1);
-                totalIncome.innerHTML = `${currencyFormatter.format( myIncomeTotal(undeletedvalue,-deletedvalue) )}`;
+                totalIncome.innerHTML = `${currencyFormatter.format( myIncome(undeletedvalue) )}`;
+                wallet.innerHTML = `${currencyFormatter.format( myIncomeTotal(undeletedvalue,-deletedvalue) )}`;
                 totalExpenses.innerHTML = `${currencyFormatter.format( myExpenseTotal(-deletedvalue) )}`;
             }
         }
-        console.log(myarray);
         e.target.parentNode.remove();
     }
 });
